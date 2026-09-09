3aa765eb1b781de4a012c910c5564c7bd8f6b8b3
saberbadri24-tech <saberbadri24@gmail.com>
2026-09-09 03:56:44 +0330

    ANIL X: update service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='anilx-v2';
+const ASSETS=['./','./index.html','./admin.html','./style.css','./script.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match('./index.html'))))});
